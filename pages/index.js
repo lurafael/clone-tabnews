63cf375c95b58311901ddd3a15e0b40622b05c61
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2025-02-05 11:24:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
 function Home() {
-    return <div style={{ padding: '0 15px' }}>
-        <h2>Olá, bom dia minha princesa!</h2>
-        <p>
-            Obrigado por tudo o que vc faz por mim,
-            sou grato demais por ter você na minha vida!
-            <br />
-            Espero que tenha acordado bem e tenha um bom dia no trabalho 🥰
-            <br />
-            <br />
-            <br />
-            <i>Obs: Espero que a semana passe rápido para a gente ficar juntos novamente e eu te tratar como no vídeo abaixo</i> ❤️ <i>(não sou o velho calvo)</i>
-        </p>
-        <div>
-            <blockquote class="twitter-tweet">
-                <p lang="pt" dir="ltr">homem das cavernas puxando mulher pelo cabelo pica pau
-                    <a href="https://t.co/wqqIFYeHKM">pic.twitter.com/wqqIFYeHKM</a>
-                </p>&mdash; acervo archive memes (@breakupwithdoja)
-                <a href="https://twitter.com/breakupwithdoja/status/1402785188304896001?ref_src=twsrc%5Etfw">June 10, 2021</a>
-            </blockquote> 
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
+  return (
+    <div style={{ padding: "0 15px" }}>
+      <h2>Olá, bom dia minha princesa!</h2>
+      <p>
+        Obrigado por tudo o que vc faz por mim, sou grato demais por ter você na
+        minha vida!
+        <br />
+        Espero que tenha acordado bem e tenha um bom dia no trabalho 🥰
+        <br />
+        <br />
+        <br />
+        <i>
+          Obs: Espero que a semana passe rápido para a gente ficar juntos
+          novamente e eu te tratar como no vídeo abaixo
+        </i>{" "}
+        ❤️ <i>(não sou o velho calvo)</i>
+      </p>
+      <div>
+        <blockquote class="twitter-tweet">
+          <p lang="pt" dir="ltr">
+            homem das cavernas puxando mulher pelo cabelo pica pau
+            <a href="https://t.co/wqqIFYeHKM">pic.twitter.com/wqqIFYeHKM</a>
+          </p>
+          &mdash; acervo archive memes (@breakupwithdoja)
+          <a href="https://twitter.com/breakupwithdoja/status/1402785188304896001?ref_src=twsrc%5Etfw">
+            June 10, 2021
+          </a>
+        </blockquote>
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+      </div>
     </div>
+  );
 }
 
 export default Home;
